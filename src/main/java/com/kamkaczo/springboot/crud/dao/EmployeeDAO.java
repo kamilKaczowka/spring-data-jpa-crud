fc7fffcdea5a4c2bc9ba5020becf14dfd405a746
Kamil Kaczowka <s21790@pjwstk.edu.pl>
2021-01-06 21:52:48 +0100

    Initial commit

---
+++ src/main/java/com/kamkaczo/springboot/crud/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.kamkaczo.springboot.crud.dao;
+
+import com.kamkaczo.springboot.crud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+
+}
---
+++ src/main/java/com/kamkaczo/springboot/crud/service/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.kamkaczo.springboot.crud.service;
+
+import com.kamkaczo.springboot.crud.dao.EmployeeDAO;
+import com.kamkaczo.springboot.crud.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public void save(Employee theEmployee) {
+        employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+}
